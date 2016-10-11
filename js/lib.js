bb0341f6f8fc0ca9eaa5d7bd843094d5c49c88df
shanzha <343190282@qq.com>
2016-10-11 09:41:03 +0800

    手机
    泡菜

---
+++ js/lib.js
@@ -0,0 +1,12 @@
+﻿function slideNav(ele){ // #nav li //下拉菜单
+	$(ele).hover(function(){           
+		$(this).find("dl").stop().slideToggle();
+		$(this).toggleClass("current");
+	});
+}
+$(document).ready(function(){
+
+        $(".ul-park  li.on").click(function(){
+                $(".park-list").toggle();
+       });
+});
